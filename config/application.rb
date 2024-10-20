9310feb568253199f994bd0db2259671c4bb4275
Pedro Souza <opedrosouza.dev@gmail.com>
2024-10-19 21:12:25 -0300

    feat: configuring rspec and spec gems

--- config/application.rb
+++ config/application.rb
@@ -20,25 +20,16 @@ Bundler.require(*Rails.groups)
 
 module Popmenu
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
-
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.test_framework :rspec
+      g.view_specs false
+      g.helper false
+    end
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    config.generators.system_tests = nil
     config.api_only = true
   end
 end
