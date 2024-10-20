12e2492d2c24667a0a8705f0b3caae3279ae00f1
Pedro Souza <opedrosouza.dev@gmail.com>
2024-10-19 23:59:49 -0300

    feat: create restaurant model

---
+++ spec/models/restaurant_spec.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: restaurants
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require "rails_helper"
+
+RSpec.describe Restaurant, type: :model do
+  it "has a valid factory" do
+    expect(build(:restaurant)).to be_valid
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+  end
+end
